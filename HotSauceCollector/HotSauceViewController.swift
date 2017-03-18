2543199b29af2f6d6ee20aefa66b6ad62492ffab
Justin Beuck <jbeuck@yahoo.com>
2017-03-17 20:59:45 -0500

    added functions and outlets to hotsauceviewcontroller

---
+++ HotSauceCollector/HotSauceViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HotSauceViewController.swift
+//  HotSauceCollector
+//
+//  Created by JUSTIN MICHAEL BEUCK on 3/17/17.
+//  Copyright © 2017 JustinCo. All rights reserved.
+//
+
+import UIKit
+
+class HotSauceViewController: UIViewController {
+
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var hotSauceImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
